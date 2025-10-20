4a2d0058f176e76839388fc0a6f2046d2bb99bed
marcosla40 <marcosla40@educastur.es>
2025-10-20 20:47:25 +0200

    Contenido clase Bienvenida

--- src/main/java/com/example/biblioteca/BienvenidaController.java
+++ src/main/java/com/example/biblioteca/BienvenidaController.java
@@ -1,5 +1,14 @@
 package com.example.biblioteca;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Una clase que va a responder a peticiones HTTP
+@RequestMapping("/") // La ruta que vamos a tener que escribir en el navegador para que se ejecute
 public class BienvenidaController {
-    
+
+    @GetMapping // Cuando en la URL hagamos una peticion Get, entre parentesis podriamos especificar la ruta: @GetMapping("")
+    public String bienvenida() { return "Bienvenid@ al IES Monte Naranco";}
 }
