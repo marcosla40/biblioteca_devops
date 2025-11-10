6bc716b92fa26e957fa8751e381a797e72ab9b72
marcosla40 <marcosla40@educastur.es>
2025-11-10 19:29:03 +0100

    LibroController

--- src/main/java/com/example/biblioteca/repository/LibroRepository.java
+++ src/main/java/com/example/biblioteca/repository/LibroRepository.java
@@ -6,11 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface LibroRepository extends JpaRepository<Libro, Long> {
-    /*
-    @Query(name = "SELECT l FROM Libro l")
-    List<Libro> listaLibros();
-    */
+
+    @Override
+    List<Libro> findAll();
+
+    @Override
+    Optional<Libro> findById(Long aLong);
 }
