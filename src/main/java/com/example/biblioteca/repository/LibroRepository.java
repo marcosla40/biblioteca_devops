083e752c701a8699807331739f25e3e3d7c0c53f
marcosla40 <marcosla40@educastur.es>
2025-11-10 20:30:07 +0100

    Quitar el comentario del LibroRepository

--- src/main/java/com/example/biblioteca/repository/LibroRepository.java
+++ src/main/java/com/example/biblioteca/repository/LibroRepository.java
@@ -2,18 +2,9 @@ package com.example.biblioteca.repository;
 
 import com.example.biblioteca.entity.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface LibroRepository extends JpaRepository<Libro, Long> {
 
-    @Override
-    List<Libro> findAll();
-
-    @Override
-    Optional<Libro> findById(Long aLong);
 }
